30e9ba45e792f85b7906f6da52a4e66fd605993c
3mmar7amed <ammarhamed64@gmail.com>
2022-08-15 15:14:22 +0200

    fix attendence

--- server/sql/migrations/002-attendence.sql
+++ server/sql/migrations/002-attendence.sql
@@ -2,10 +2,13 @@
 /*
 SQLite does not have a storage class for storing dates and/or times. Instead, 
 the built-in Date and Time Functions of SQLite are capable of storing dates and times as TEXT, REAL, or INTEGER
+**/
+
+
 
 CREATE TABLE attendence (
 
-    id **/
+    id 
          VARCHAR PRIMARY KEY , 
     st_id        VARCHAR NOT NULL , 
     date         INTEGER NOT NULL , 
