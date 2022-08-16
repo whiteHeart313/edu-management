089d5b4ebc028ee2d339d1b2f4ca9a3462e63d54
abdulrhmanalqassas <46942535+abdulrhmanalqassas@users.noreply.github.com>
2022-08-16 19:52:50 +0200

    modifie and start add exam

---
+++ frontend/src/components/Exam.js
@@ -0,0 +1,78 @@
+import * as React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { axiosPublic } from "../api/axiosPublic";
+import { Button } from "@mui/material";
+
+const columns = [
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "name", headerName: "الإسم", width: 130 },
+  { field: "grade", headerName: "السنه", width: 130 },
+  { field: "group_", headerName: "المجموعه", width: 130 },
+  { field: "phone", headerName: "رقم الهاتف", type: "number", width: 150 },
+  { field: "type", headerName: "المدرسه", width: 160 },
+];
+
+export default function Exam() {
+  const [ids, setIds] = React.useState([]);
+  const [students, setStudents] = React.useState([]);
+
+//   const SelectionHandeler = (id) => {
+//     setAtttend((oldId) => {
+//       return oldId.push({ id: id });
+//     });
+//   };
+  React.useEffect(() => {
+    axiosPublic
+      .get("/getStudents")
+      .then((res) => setStudents(res.data.students))
+      .then((err) => console.log(err));
+  }, []);
+
+  const idsFormater = (ids) => {
+    let arr = [];
+    for (let i = 0; i < ids.length; i++) {
+      console.log({ id: ids[i] });
+
+      arr.push({ id: ids[i] });
+    }
+    return arr;
+  };
+  const examHandeler = () => {
+    axiosPublic
+      .post("/createExam", {
+        studentsResults: [
+          {
+            st_id: "9dc091f0-1a1f-4f85-b8d8-340a71567f12",
+            examResult: 5,
+          },
+        ] , 
+        examType : true , // dialy eam 
+        date : "7/9/2022"
+        ,
+      })
+      .then((res) => console.log(res))
+      .then((err) => console.log(err));
+  };
+  return (
+    <div style={{ height: 500, width: "100%" }}>
+      <DataGrid
+        rows={students}
+        columns={columns}
+        pageSize={10}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+        onSelectionModelChange={(e) => {
+          setIds(e);
+        }}
+        isRowSelectable={(params) => {
+          //  let x =  axiosPublic.
+          //  post("/atttendStudent",{studentsIds:[{id:params.row.id}]})
+          //  .then((res)=>  true)
+          //  .then(err=>  false)
+          return true;
+        }}
+      />
+      <Button onClick={() => examHandeler()}> take exam ruselt </Button>
+    </div>
+  );
+}
