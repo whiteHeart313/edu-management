6bf52b3824e99172ad74b44730c1d2bcbf7cfd8a
abdulrhmanalqassas <46942535+abdulrhmanalqassas@users.noreply.github.com>
2022-08-24 19:29:07 +0200

    test all ind point pst or git (done)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,49 @@ import { Box } from "@mui/system";
 import SideNav from "../src/components/Drawer";
 import { testEndPoints } from './components/test';
 
-const postArr = ["atttendStudent",]
+const postArr = ["/atttendStudent",
+'/getExamByMonth' ,
+'/addStudent', 
+'/deleteStudent',
+'/changeGroup',
+'/createExam']
+const postReq = [
+  {studentsIds:[{id:"007ebbc0-ba27-4c6f-a03a-962f7ab95b73"}],},
+  {month:"7"},
+  {
+    name:"any hit ",
+    phone:"0555552" ,
+    parentPhone:"0465418334512" ,
+    grade:" اولي ثانوي", 
+    group:" اولي ثانوي",
+    type:" اولي ثانوي",
+  },
+  {id:"1d42946f-fafa-4c1e-857a-90115cec3064"},
+  { id: "007ebbc0-ba27-4c6f-a03a-962f7ab95b73",switchedGroup: "جمعه" },
+  {
+    "studentsResults": 
+    [
+      {
+        "exam" : {
+          "st_id": "007ebbc0-ba27-4c6f-a03a-962f7ab95b73",
+          "examResult": 5
+        } 
+      }  
+      
+    ] , 
+    "ExamType" : true  , 
+    "date" : "7/9/2022"
+    
+}
+
+];
+
 const getArr = ["/getStudents","getTodaysAttendence","getMonthlyMoneyForAllStudents"]
 
 function App() {
-  testEndPoints(getArr, "get")
+console.log(testEndPoints(postArr, "post",postReq))
+
+// console.log(">>>>>>>>>>>>get test ",testEndPoints(getArr,"get"))
   return (
     <div className="App">
       <SideNav />
--- frontend/src/components/test.js
+++ frontend/src/components/test.js
@@ -2,21 +2,25 @@ import { axiosPublic } from "../api/axiosPublic";
 
 export function testEndPoints(endpoint = ["/"], reqtype = "get", postData =[{}]) {
   let index =  0 
+  let reqArr = []
   for (let url of endpoint) {
     
     if (reqtype === "get") {
       axiosPublic
       .get(url)
-        .then((res) => console.log(`${index } - get req res for : ${url}`,res.data))
-        .then((err) => console.log(`${index } - get req error for : ${url}`,err));
+        .then((res) => reqArr.push(`${index } - get req res for : ${url}`,res.data))
+        .then((err) => reqArr.push(`${index } - get req error for : ${url}`,err));
     } else if (reqtype === "post") {
       postData ? axiosPublic
-      .post(endpoint, postData[index])
-      .then((res) => console.log(`${index} - post req for : ${url}`,res.data))
-      .then((err) => console.log(`${index} - post req for : ${url}`,err)) 
-      : console.log("no data provid req data for post req")
+      .post(url, postData[index])
+      .then((res) => reqArr.push(`${index} - post req for : ${url}`,res.data))
+      .then((err) => reqArr.push(`${index} - post req  error for : ${url}`,err)) 
+      :reqArr.push("no data provid req data for post req")
       
     }
     index ++
   }
+  return reqArr
 }
+
+
